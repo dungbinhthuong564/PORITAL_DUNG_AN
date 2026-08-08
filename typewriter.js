4de907b7d58728765c83e4c1168ad3a01911d5f1
dungbinhthuong564 <dungbinhthuong564@gmail.com>
2026-08-08 08:54:15 +0700

    Initialize project in portfolio

--- typewriter.js
+++ typewriter.js
@@ -7,7 +7,6 @@ let isDeleting = false;
 function typeEffect() {
   if (!typingElement) return;
 
-  // Gõ từng chữ
   if (!isDeleting && index <= text.length) {
     typingElement.textContent = text.slice(0, index++);
     setTimeout(typeEffect, 100);
@@ -17,16 +16,14 @@ function typeEffect() {
     typingElement.textContent = text.slice(0, index--);
     setTimeout(typeEffect, 60);
   }
-  // Sau khi gõ xong, chuyển sang xóa
   else if (!isDeleting && index > text.length) {
     isDeleting = true;
-    setTimeout(typeEffect, 1000); // đợi 1 giây rồi bắt đầu xóa
+    setTimeout(typeEffect, 1000); 
   }
-  // Sau khi xóa xong, bắt đầu lại mà KHÔNG hiển nguyên dòng
   else if (isDeleting && index < 0) {
     isDeleting = false;
     index = 0;
-    setTimeout(typeEffect, 500); // đợi 0.5s rồi gõ lại
+    setTimeout(typeEffect, 500); 
   }
 }
 
